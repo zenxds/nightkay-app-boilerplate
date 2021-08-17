815268b3a0bd5502540e41ea7fedc8d27ffee143
zenxds <zenxds@gmail.com>
2021-08-17 17:06:36 +0800

    feat: prettier文件与hawk统一

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  parser: 'typescript',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+}
